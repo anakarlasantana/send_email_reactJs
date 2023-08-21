a94b35374fd5bbc87571cb3a05aea640af745c2f
anakarlasantana <santanadeanakarla@gmail.com>
2023-08-21 15:15:30 -0300

    feat: adição de modal de envio de email

--- src/components/ForgotPassword/index.tsx
+++ src/components/ForgotPassword/index.tsx
@@ -1,24 +1,31 @@
 import React, { useRef, useState } from 'react';
 import { Link } from 'react-router-dom';
-
+import { Modal, Button } from 'react-bootstrap';
 import './styles.css';
 import { postForgotPassword } from '../../pages/services';
 
 const ForgotPasswordForm: React.FC = () => {
     const formRef = useRef<HTMLFormElement>(null);
     const [loading, setLoading] = useState(false);
+    const [sendpassword, setSendpassword] = useState(false);
+    const [formData, setFormData] = useState({
+        email: '',
+    });
+
+
 
     const ForgotPassword = (userData: { email: string }) => {
-        setLoading(true); 
+        setLoading(true);
         postForgotPassword(userData)
             .then((res) => {
                 console.log("Resposta da requisição:", res);
+                setSendpassword(true);
             })
             .catch((error) => {
                 console.error("Erro na requisição:", error);
             })
             .finally(() => {
-                setLoading(false); 
+                setLoading(false);
             });
     };
 
@@ -35,6 +42,13 @@ const ForgotPasswordForm: React.FC = () => {
         }
     };
 
+    const handleCloseModal = () => {
+        setSendpassword(false)
+        setFormData({
+            email: '',
+        });
+    }
+
     return (
         <div className="container">
             <h1>Recuperar Senha</h1>
@@ -44,7 +58,9 @@ const ForgotPasswordForm: React.FC = () => {
             <div className="form-login">
                 <form ref={formRef} onSubmit={handleSubmit}>
                     <label>Email</label>
-                    <input type="email" name="email" className="custom-input" required />
+                    <input type="email" name="email" className="custom-input" required
+                        value={formData.email} onChange={(e) => setFormData({ ...formData, email: e.target.value })}
+                    />
                     <button className="custom-buttom" type="submit" disabled={loading}>
                         {loading ? <span role="status">Enviando...</span> : 'Enviar'}
                     </button>
@@ -56,7 +72,19 @@ const ForgotPasswordForm: React.FC = () => {
                     </div>
                 </form>
             </div>
+            <Modal show={sendpassword} centered onHide={() => setSendpassword(false)}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Email enviado!</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>Foi enviado ao seu email, o procedimento de recuperação de senha.</Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleCloseModal}>
+                        Fechar
+                    </Button>
+                </Modal.Footer>
+            </Modal>
         </div>
+
     );
 };
 
